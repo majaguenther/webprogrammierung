47c4841cde24ad7d037805a5400ebe1e520d4cc2
Maja <majaguentherr@gmail.com>
2025-03-21 14:27:26 +0100

    question 1

--- src/quizes/questions.js
+++ src/quizes/questions.js
@@ -1,16 +1,16 @@
 export const questions1 = [
-    { title: "Titel2" },
-    { question: "Was ist die Hauptstadt von Deutschland?", options: ["Berlin", "München", "Hamburg", "Köln"], answer: "Berlin" },
-    { question: "Welches Element hat das chemische Symbol O?", options: ["Gold", "Sauerstoff", "Silber", "Helium"], answer: "Sauerstoff" },
-    { question: "Wie viele Kontinente gibt es?", options: ["5", "6", "7", "8"], answer: "7" },
-    { question: "Wer schrieb 'Faust'?", options: ["Goethe", "Schiller", "Lessing", "Kafka"], answer: "Goethe" },
-    { question: "Was ist die Wurzel von 64?", options: ["6", "7", "8", "9"], answer: "8" },
-    { question: "Welcher Planet ist der größte im Sonnensystem?", options: ["Mars", "Venus", "Jupiter", "Saturn"], answer: "Jupiter" },
-    { question: "Wie viele Beine hat eine Spinne?", options: ["6", "8", "10", "12"], answer: "8" },
-    { question: "Was ist die kleinste Einheit eines Computerspeichers?", options: ["Bit", "Byte", "Kilobyte", "Megabyte"], answer: "Bit" },
-    { question: "Welche Farbe hat Chlorophyll?", options: ["Rot", "Gelb", "Grün", "Blau"], answer: "Grün" },
-    { question: "Wie viele Monate haben 28 Tage?", options: ["1", "6", "12", "9"], answer: "12" },
-  ];
+  { title: "Hauptsätdte raten!" },
+  { question: "Welche ist die Hauptstadt von Deutschland?", options: ["Berlin", "München", "Hamburg", "Köln"], answer: "Berlin" },
+  { question: "Welche ist die Hauptstadt von Lettland?", options: ["Liepaja", "Jelgava", "Riga", "Windau"], answer: "Riga" },
+  { question: "Welche ist die Hauptstadt der Türkei?", options: ["Antalya", "Izmir", "Ankara", "Istanbul"], answer: "Ankara" },
+  { question: "Welche ist die Hauptstadt von Indonesien?", options: ["Surabaya", "Jakarta", "Bandung", "Semarang"], answer: "Jakarta" },
+  { question: "Welche ist die Hauptstadt von Kuba?", options: ["Havanna", "Santiago de Cuba", "Camagüey", "Holgum"], answer: "Havanna" },
+  { question: "Welche ist die Hauptstadt vom SUdan?", options: ["Omdurman", "al-Chartum Bahri", "Nyala", "Khartum"], answer: "Khartum" },
+  { question: "Welche ist die Hauptstadt von Malaysia?", options: ["Kuala Lumpur", "Kota Kinabalu", "Kuching", "Johor Bahru"], answer: "Kuala Lumpur" },
+  { question: "Welche ist die Hauptstadt der Vereinigten Arabischen Emirate?", options: ["Abu Dhabi", "Schardscha", "Dubai", "Al-Ain"], answer: "Dubai" },
+  { question: "Welche ist die Hauptstadt vom Kongo?", options: ["Kinhasa", "Bukavu", "Mbuji-Mayi", "Kisangani"], answer: "Kinhasa" },
+  { question: "Welche ist die Hauptstadt von Weissrussland?", options: ["Wizebsk", "Brest", "Pinsk", "Minsk"], answer: "Minsk" },
+];
   
   export const questions2 = [
     { title: "Titel!"},
