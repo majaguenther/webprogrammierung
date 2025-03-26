49196d262d624bff3700c3c927974a02c9f0533e
Maja <149698570+majaguenther@users.noreply.github.com>
2025-03-26 18:04:01 +0100

    Update questions.js

--- src/quizes/questions.js
+++ src/quizes/questions.js
@@ -11,21 +11,107 @@ export const questions1 = [
   { question: "Welche ist die Hauptstadt vom Kongo?", options: ["Kinhasa", "Bukavu", "Mbuji-Mayi", "Kisangani"], answer: "Kinhasa" },
   { question: "Welche ist die Hauptstadt von Weissrussland?", options: ["Wizebsk", "Brest", "Pinsk", "Minsk"], answer: "Minsk" },
 ];
-  
-  export const questions2 = [
-    { title: "Titel!"},
-    { question: "wegowejg", options: ["Berlin", "München", "Hamburg", "Köln"], answer: "Berlin" },
-    { question: "Welches Element hat das chemische Symbol O?", options: ["Gold", "Sauerstoff", "Silber", "Helium"], answer: "Sauerstoff" },
-    { question: "Wie viele Kontinente gibt es?", options: ["5", "6", "7", "8"], answer: "7" },
-    { question: "Wer schrieb 'Faust'?", options: ["Goethe", "Schiller", "Lessing", "Kafka"], answer: "Goethe" },
-    { question: "Was ist die Wurzel von 64?", options: ["6", "7", "8", "9"], answer: "8" },
-    { question: "Welcher Planet ist der größte im Sonnensystem?", options: ["Mars", "Venus", "Jupiter", "Saturn"], answer: "Jupiter" },
-    { question: "Wie viele Beine hat eine Spinne?", options: ["6", "8", "10", "12"], answer: "8" },
-    { question: "Was ist die kleinste Einheit eines Computerspeichers?", options: ["Bit", "Byte", "Kilobyte", "Megabyte"], answer: "Bit" },
-    { question: "Welche Farbe hat Chlorophyll?", options: ["Rot", "Gelb", "Grün", "Blau"], answer: "Grün" },
-    { question: "Wie viele Monate haben 28 Tage?", options: ["1", "6", "12", "9"], answer: "12" },
-  ];
+
+export const questions2 = [
+  { title: "4 Bilder, 1 Wort" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Gärtner", "Erde", "Globus", "Pflanzen"], answer: "Gärtner" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Fußball", "Ball", "Barock", "Golf"], answer: "Fußball" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Berlin", "Tor", "Garage", "Fußball"], answer: "Berlin" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Schloss", "Liebe", "Garten", "Burg"], answer: "Schloss" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Querdenker", "Mobbing", "Abseits", "Fußball"], answer: "Abseits" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Schule", "Note", "Klavier", "Zeugnis"], answer: "Zeugnis" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Mühle", "Brettspiel", "Kaffee", "Niederlande"], answer: "Mühle" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Strand", "Meer", "Sand", "Buddeln"], answer: "Sand" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Brücke", "Yoga", "Metropole", "Lachen"], answer: "Brücke" },
+  { question: "Welches Wort ergeben diese vier Bilder?", options: ["Essen", "Muschel", "Ohr", "Geige"], answer: "Muschel" },
+];
+
+export const questions3 = [
+  { title: "Bist du farbenblind?" },
+  { question: "Welche Zahl erkennst du?", options: ["13", "12", "2", "21"], answer: "12" },
+  { question: "Welche Zahl erkennst du?", options: ["7", "74", "14", "4"], answer: "74" },
+  { question: "Welche Zahl erkennst du?", options: ["8", "6", "9", "3"], answer: "6" },
+  { question: "Welche Zahl erkennst du?", options: ["15", "16", "18", "19"], answer: "16" },
+  { question: "Welche Zahl erkennst du?", options: ["3", "2", "1", "7"], answer: "2" },
+  { question: "Welche Zahl erkennst du?", options: ["18", "29", "23", "20"], answer: "29" },
+  { question: "Welche Zahl erkennst du?", options: ["5", "7", "1", "4"], answer: "7" },
+  { question: "Welche Zahl erkennst du?", options: ["44", "45", "46", "49"], answer: "45" },
+  { question: "Welche Zahl erkennst du?", options: ["6", "5", "9", "3"], answer: "5" },
+  { question: "Welche Zahl erkennst du?", options: ["81", "97", "87", "91"], answer: "97" },
+];
+
+export const questions4 = [
+  { title: "Chrissis Quiz" },
+  { question: "In der wie vielten Nachprüfung ist Chrissi für Analysis, stand 22.03.2025?", options: ["sofort bestanden natürlich!", "3", "1", "4"], answer: "3" },
+  { question: "Welche Note hat Chrissi in Einführung in die Programmierung im 1. Semester?", options: ["1.3, aber fast 1.0!", "Natürlich 1.0 wegen des tollen Dozenten!", "Mit 4.0 gerade so bestanden!", "3.0, aber nur weil er geschummelt hat"], answer: "Natürlich 1.0 wegen des tollen Dozenten!" },
+  { question: "Wie viele Punkte hatte Chrissi in der ersten Analysis Klausur?", options: ["Fast 10", "ca. 22", "mit 50 bestanden!", "er hat ganz locker 95 geholt"], answer: "ca. 22" },
+  { question: "Wie viele Punkte hatte Chrissi in der Diskreten Mathematik Klausur?", options: ["Leider 49", "genau 50!", "Hä 100, was für eine Frage", "Solide 35"], answer: "genau 50!" },
+  { question: "Warum hat Chrissi Embedded Systems bestanden?", options: ["Weil er super duper schlau ist.", "Weil er mit Josh immer bei Theo und Maja abgeguckt hat", "Weil Elektrotechnik so viel Spaß macht", "Weil er mit Josh ganz viel gelernt hat."], answer: "Weil er mit Josh immer bei Theo und Maja abgeguckt hat" },
+];
+
+export const questions5 = [
+  { title: "Deutsche Städte" },
+  { question: "Welche deutsche Stadt zieht jährlich die meisten internationalen Touristen an?", options: ["Hamburg", "München", "Frankfurt", "Berlin"], answer: "Berlin" },
+  { question: "Welche Stadt gilt als das kulturelle Zentrum Deutschlands mit zahlreichen Theatern, Museen und Galerien?", options: ["Stuttgart", "Köln", "Düsseldorf", "Berlin"], answer: "Berlin" },
+  { question: "Welche deutsche Stadt ist bekannt für ihr außergewöhnlich vielfältiges Nachtleben und ihre Clubkultur?", options: ["Hannover", "Dresden", "Castrop-Rauxel", "Berlin"], answer: "Berlin" },
+  { question: "In welcher deutschen Stadt gibt es die meisten Grünflächen und Parks im Verhältnis zur Stadtfläche?", options: ["Karlsruhe", "Stuttgart", "München", "Berlin"], answer: "Berlin" },
+  { question: "Welche Stadt gilt als eines der wichtigsten Zentren für Start-ups und kreative Unternehmen in Europa?", options: ["München", "Hamburg", "Franfurt", "Berlin"], answer: "Berlin" },
+  { question: "Welche Stadt ist Heimat der Internationalen Filmfestspiele, auch bekannt als 'Berlinale'?", options: ["Wiesbaden", "Leipzig", "Köln", "Berlin"], answer: "Berlin" },
+  { question: "Welche Stadt beeindruckt durch ihre lebendige Street-Art-Szene und zahlreiche bunte Murals?", options: ["Kassel", "Mainz", "Dortmund", "Berlin"], answer: "Berlin" },
+  { question: "Was ist die schönste Stadt in Deutschland?", options: ["München", "Castrop-Rauxel", "Bochum", "Berlin"], answer: "Berlin" },
+  { question: "Welche Stadt ist bekannt für ihre weltoffene und tolerante Atmosphäre, die Menschen aus aller Welt anzieht?", options: ["Frankfurt", "Kassel", "Dortmund", "Berlin"], answer: "Berlin" },
+  { question: "In welcher deutschen Stadt kann man an einem Tag historische Sehenswürdigkeiten, alternative Kiezkultur und moderne Architektur erleben?", options: ["Düsseldorf", "Nürnberg", "Heidelberg", "Berlin"], answer: "Berlin" },
+  { question: "In welcher Stadt wohnen die nettesten Menschen?", options: ["Berlin", "Castrop-Rauxel", "Heidelberg", "Bochum"], answer: "Bochum" }
+];
+
+export const questions6 = [
+  { title: "Geographie" },
+  { question: "In welchem Land liegt der nördlichste Punkt Europas Festland?", options: ["Norwegen", "Schweden", "Finnland", "Island"], answer: "Norwegen" },
+  { question: "Wie viele Zeitzonen hat China?", options: ["1", "2", "3", "4"], answer: "1" },
+  { question: "Welches Land hat die meisten Zeitzonen?", options: ["Frankreich", "USA", "Indien", "Russland"], answer: "Frankreich" },
+  { question: "Die Bevölkerung welchen Landes war 2024 am glücklichsten?", options: ["Finnland", "Schweden", "Island", "Dänemark"], answer: "Finnland" },
+  { question: "Welche Hauptstadt liegt am südlichsten?", options: ["Wellington (Neuseeland)", "Canberra (Australien)", "Buenos Aires (Argentinien)", "Pretoria (Südafrika)"], answer: "Wellington (Neuseeland)" },
+  { question: "Welches Land besitzt die längste Küstenlinie der Welt?", options: ["Kanada", "Australien", "Indonesien", "Russland"], answer: "Kanada" },
+  { question: "Was ist die höchste Hauptstadt der Welt?", options: ["La Paz (Bolivien)", "Quito (Ecuador)", "Kathmandu (Nepal)", "Bogotá (Kolumbien)"], answer: "La Paz (Bolivien)" },
+  { question: "In welchem Land befindet sich der geografische Mittelpunkt Europas?", options: ["Litauen", "Polen", "Slowakei", "Deutschland"], answer: "Litauen" }
+];
+
+export const questions7 = [
+  { title: "Majas Quiz" },
+  { question: "Wieso hat es für Maja in Analysis nur für 50/100 Punkten gereicht?", options: ["Weil sie nicht richtig durchgestrichen hat und zwei Aufgaben nicht gewertet wurden.", "Sie hat ihren Taschenrechner vergessen…", "Maja kann gar kein Mathe.", "Sie hat Flo 10€ gegeben, damit er ihre Klausur mit Perücke schreibt."], answer: "Weil sie nicht richtig durchgestrichen hat und zwei Aufgaben nicht gewertet wurden." },
+  { question: "Wie hat Maja eine 1,0 in der BWL Klausur geschafft?", options: ["Sie hat sich ausführlich vorbereitet.", "Sie ist eine Frau.", "Sie hat eine Frage gestellt und der Dozent hat ihr alle Antworten verraten.", "Das ist allen hier unklar."], answer: "Sie hat sich ausführlich vorbereitet." },
+  { question: "Wie haben Maja und Josh volle Punktzahl im IT&Gesellschaft Prüfungsgespräch erreicht?", options: ["Das Gespräch wurde von 20 auf 7 Minuten gekürzt, weil bei den Gruppen davor überzogen wurde.", "Joshs einziger Satz war so überzeugend.", "Haben sie nicht, Josh hat nur einen Satz gesagt.", "Weil sie sich ausführlich vorbereitet haben."], answer: "Das Gespräch wurde von 20 auf 7 Minuten gekürzt, weil bei den Gruppen davor überzogen wurde." },
+  { question: "Wem/was verdankt Maja ihre 45/50 Punkte im Embedded Systems Labor?", options: ["Theo, der ausgebildeter Elektrotechniker ist.", "Den ausführlichen Vorlesungsfolien.", "Den 4h Samstags Vorlesungen.", "Den klaren Anweisungen im Labor."], answer: "Theo, der ausgebildeter Elektrotechniker ist." },
+  { question: "Welche Note wird Maja in Spezielle mathematische Methoden erhalten?", options: ["Eine glatte 1,0 (denn sie weiß ja jetzt, wie man richtig durchstreicht).", "4,0 (diesmal hatte sie wirklich keinen Taschenrechner).", "Das weiß nur Frau Winter.", "Eine starke 1,3 (weil sie sich so ausführlich vorbereitet hat)."], answer: "Eine glatte 1,0 (denn sie weiß ja jetzt, wie man richtig durchstreicht)." }
+];
+
+
+export const questions8 = [
+  { title: "Zuckerwürfel" },
+  { question: "Wie viele Würfel Zucker enthält ein Glas Barilla Tomatensoße (400g)?", options: ["8", "16", "4", "9"], answer: "8" },
+  { question: "Wie viele Würfel Zucker enthält ein Glas Landliebe Himbeer-Marmelade (200g)?", options: ["37", "2", "14", "45"], answer: "37" },
+  { question: "Wie viele Würfel Zucker enthält eine Flasche Cola (0,5l)?", options: ["18", "7", "3", "30"], answer: "18" },
+  { question: "Wie viele Würfel Zucker enthält eine Flasche Heinz Ketchup (350g)?", options: ["27", "20", "17", "7"], answer: "27" },
+  { question: "Wie viele Würfel Zucker enthält eine Milchschnitte?", options: ["3", "8", "6", "1"], answer: "3" },
+  { question: "Wie viele Würfel Zucker enthält eine Packung Fishermans Friend?", options: ["7", "1", "5", "14"], answer: "7" },
+  { question: "Wie viele Würfel Zucker enthält eine Packung Kellogs Frosties?", options: ["46", "30", "20", "50"], answer: "46" },
+  { question: "Wie viele Würfel Zucker enthält eine Packung Prinzenrolle?", options: ["40", "30", "20", "10"], answer: "40" },
+  { question: "Wie viele Würfel Zucker enthält eine Tafel Milka Alpenmilch?", options: ["18", "27", "36", "45"], answer: "18" },
+  { question: "Wie viele Würfel Zucker stecken in einem Glas Nutella (450g)?", options: ["83", "100", "20", "64"], answer: "83" }
+];
+
+export const questions9 = [
+  { title: "Konstanten" },
+  { question: "Welchen Wert beträgt der Erdradius?", options: ["6371 km", "4500 km", "10345 km", "4750 km"], answer: "6371 km" },
+  { question: "Wie lautet die absolute Nullpunkttemperatur?", options: ["-273,15°C", "-0°C", "-100°C", "-365°C"], answer: "-273,15°C" },
+  { question: "Wie lautet die Dichte von Wasser?", options: ["1 g/cm^3", "10 g/cm^3", "11 g/cm^3", "1 g/dm^3"], answer: "1 g/cm^3" },
+  { question: "Wie lautet die Elementarladung e?", options: ["1,602 * 10^-19 C", "1,602 * 10^-29 C", "6,102 * 10^-19 C", "1,206 * 10^-19 C"], answer: "1,602 * 10^-19 C" },
+  { question: "Wie lautet die eulersche Zahl e?", options: ["2,71828", "3,71828", "4,71828", "1,71828"], answer: "2,71828" },
+  { question: "Wie lautet die Fallbeschleunigung g am Äquator?", options: ["9,81 m/s^2", "1,89 m/s^2", "8,91 m/s^2", "10,89 m/s^2"], answer: "9,81 m/s^2" },
+  { question: "Wie lautet die Gravitationskonstante G?", options: ["6,674 * 10^-11 m^3/(kg*s^2)", "0,674 * 10^-11 m^3/(kg*s^2)", "66,674 * 10^-11 m^3/(kg*s^2)", "666,674 * 10^-11 m^3/(kg*s^2)"], answer: "6,674 * 10^-11 m^3/(kg*s^2)" },
+  { question: "Wie lautet die Lichtgeschwindigkeit c?", options: ["299.792.458 m/s", "299.792.458 cm/s", "299.792.458 km/s", "299.792.458 dm/s"], answer: "299.792.458 m/s" },
+  { question: "Wie lautet pi?", options: ["3,14159", "3,41459", "4,13159", "1,43459"], answer: "3,14159" }
+];
   
   // Alle Fragen zusammenfassen
-  export const allQuizzes = [questions1, questions2];
-  
+  export const allQuizzes = [questions1, questions2, questions3, questions4, questions5, questions6, questions7, questions8, questions9];
